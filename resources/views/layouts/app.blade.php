0677b3365f6eb665a1c2d62670e63345575720d7
Safal Shrestha <sth.safal@gmail.com>
2019-08-26 19:46:47 +0100

    Designed Logos, created basic template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <img src = images/yourmoviedb.png width="100px">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -75,6 +75,13 @@
         <main class="py-4">
             @yield('content')
         </main>
+
+        <footer class="bg-dark py-4 text-white-50">
+                <div class="container text-center">
+                  <small>Copyright &copy; YourMovieDB 2019</small>
+                </div>
+        </footer>
     </div>
 </body>
+
 </html>
